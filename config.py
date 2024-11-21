0c328a947a31aaf39852edcd9393ac523768601a
JuryReimis <80026660+JuryReimis@users.noreply.github.com>
2024-11-21 12:27:08 +0300

    Добавлены пути для файлов импровизированной базы данных

--- config.py
+++ config.py
@@ -2,4 +2,6 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent
 
-DATA_BASE_FILE = BASE_DIR / 'db/library_db.json'
+DATA_BASE_LIBRARY_PATH: Path = BASE_DIR / 'db/library_db.json'
+
+DATA_BASE_SERVICE_PATH: Path = BASE_DIR / 'db/service_data.json'

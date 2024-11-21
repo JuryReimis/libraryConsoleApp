723811c8e3c3ef7ef20c4c119aeffa91fd4eb6e6
JuryReimis <80026660+JuryReimis@users.noreply.github.com>
2024-11-21 13:06:11 +0300

    Update config.py

--- config.py
+++ config.py
@@ -5,3 +5,13 @@ BASE_DIR = Path(__file__).resolve().parent
 DATA_BASE_LIBRARY_PATH: Path = BASE_DIR / 'db/library_db.json'
 
 DATA_BASE_SERVICE_PATH: Path = BASE_DIR / 'db/service_data.json'
+
+HELLO_MESSAGE: str = """\tПриветствую вас в приложении для обслуживания библиотеки. Здесь есть несколько функций.
+\tВы можете добавлять книги, удалять их, менять их статус. Вы можете посмотреть полный список всех книг и
+\tнайти все книги по названию, автору и году издания. Приступайте к работе:"""
+
+FUNCTIONAL_DESCRIPTION: str = """\tПросмотреть список всех книг: /all
+\tДобавить книгу: /add
+\tУдалить книгу: /delete
+\tПоиск книги: /search
+\tИзменить статус книги: /change-status"""

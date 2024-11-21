7ede44e7ca55a8981e3ce1cd7cb8b28a7dc66e81
JuryReimis <80026660+JuryReimis@users.noreply.github.com>
2024-11-22 02:01:13 +0300

    В предстартовый валидатор добавлен метод проверки последнего созданного personal key

--- validators/run_validator.py
+++ validators/run_validator.py
@@ -1,7 +1,7 @@
 import json
 
-from app_orm.ORM import ORM
-from config import DATA_BASE_FILE_PATH, DATA_BASE_SERVICE_DATA_PATH
+from app_orm.ORM import ORM, BooksManager
+from config import DATA_BASE_LIBRARY_PATH, DATA_BASE_SERVICE_PATH
 
 
 class RunValidator:
@@ -15,8 +15,8 @@ class RunValidator:
 
     @classmethod
     def check_db(cls):
-        base_file = DATA_BASE_FILE_PATH
-        service_file = DATA_BASE_SERVICE_DATA_PATH
+        base_file = DATA_BASE_LIBRARY_PATH
+        service_file = DATA_BASE_SERVICE_PATH
         if not base_file.exists():
             try:
                 ORM.create_table(base_file)
@@ -30,3 +30,31 @@ class RunValidator:
             except Exception as error:
                 print("Произошла ошибка при создании служебной базы данных, обратитесь в технический отдел")
                 raise error
+
+    @classmethod
+    def check_last_generated_pk(cls):
+        service_file = DATA_BASE_SERVICE_PATH
+        last_pk = None
+
+        try:
+            books: list = BooksManager().get_all_books()
+            if books:
+                max_pk = max([book.get('id') for book in books])
+                service_data = ORM.get_data(service_file)
+                if service_data:
+                    last_pk = service_data.get('last_generated_pk')
+                if last_pk:
+                    if last_pk != max_pk:
+                        service_data['last_generated_pk'] = max_pk
+                        ORM.dump_data(service_file, service_data)
+                else:
+                    ORM.dump_data(service_file, cls.service_file_pattern)
+
+        except json.decoder.JSONDecodeError:
+            print("База данных библиотеки пуста. Решаю эту проблему")
+            ORM.dump_data(DATA_BASE_LIBRARY_PATH, dict())
+            ORM.dump_data(DATA_BASE_SERVICE_PATH, cls.service_file_pattern)
+
+        except Exception as error:
+            print("Произошла ошибка")
+            raise error

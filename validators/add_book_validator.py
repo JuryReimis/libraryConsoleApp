da87f32179b31c93ff7c0e7a17616963e18424df
JuryReimis <80026660+JuryReimis@users.noreply.github.com>
2024-11-23 21:27:03 +0300

    Переписан валидатор под новые условия.

--- validators/add_book_validator.py
+++ validators/add_book_validator.py
@@ -6,42 +6,47 @@ from utils.search import Search
 
 class AddBookValidator:
 
-    def __init__(self, title: str, author: str, year: str):
-        self.title = title
-        self.author = author
-        self.year = year
+    def __init__(self, title: str = None, author: str = None, year: str = None):
+        self._title = title
+        self._author = author
+        self._year = year
 
-    def validate(self) -> list[str]:
-        errors = []
-        year_errors = self.validate_year()
-        if year_errors:
-            errors.append(year_errors)
-        book_errors = self.validate_book()
-        if book_errors:
-            errors.append(book_errors)
-        return errors
-
-    def validate_year(self, bc_pattern: str = r'(\d+)\s*(д\.н\.э\.|до н\.э\.|BC)?(.*)') -> None | str:
-        error = 'В значении "Год" Присутствует неизвестный символ, который не является цифрой'
-        if self.year == "Неизвестно":
+    def validate_title(self, title) -> str | None:
+        self._title = title
+        search = Search()
+        search.init_search([title])
+        searched_books = search.get_searched_books()
+        if searched_books:
+            return "Уже есть книги с таким названием, проверьте, хотите ли вы продолжить"
+        else:
             return None
-        bc = re.match(bc_pattern, self.year)
+
+    def validate(self, author: str) -> None | str:
+        self._author = author
+        search = Search()
+        search.init_search(['&&&'.join((self._title, self._author, self._year))])
+        searched_books = search.get_searched_books()
+        if searched_books:
+            return (f"""!!!Вы пытаетесь добавить книгу, которая уже есть в базе данных 
+Название: {searched_books[0].get('title')}
+Автор: {searched_books[0].get('author')}
+Год издания: {searched_books[0].get('year')}
+Если здесь есть какая-то ошибка - обратитесь в технический отдел или воспользуйтесь \"Жестким\" добавлением\n""")
+        return None
+
+    def validate_year(self, year: str, bc_pattern: str = r'(\d+)\s*(д\.н\.э\.|до н\.э\.|BC)?(.*)') -> None | str:
+        self._year = year
+        error = 'В значении "Год" Присутствует неизвестный символ, который не является цифрой'
+        if year == "Неизвестно":
+            return None
+        bc = re.search(bc_pattern, year)
         if bc.group(3):
             return error
-
         for char in bc.group(1):
             if char not in digits:
                 return error
         return None
 
-    def validate_book(self) -> None | str:
-        search = Search()
-        search.init_search(["&&&".join((self.title, self.author, self.year))])
-        searched_books: list[dict] = search.get_searched_books()
-        if searched_books:
-            return (f"""!!!Вы пытаетесь добавить книгу, которая уже есть в базе данных 
-Название: {searched_books[0].get('title')}
-Автор: {searched_books[0].get('author')}
-Год издания: {searched_books[0].get('year')}"
-Если здесь есть какая-то ошибка - обратитесь в технический отдел или воспользуйтесь \"Жестким\" добавлением\n""")
-        return None
+    def transform_year(self, source: str = 'EN', target: str = 'RU') -> str:
+        return self._year.replace('BC', 'до н.э.')
+

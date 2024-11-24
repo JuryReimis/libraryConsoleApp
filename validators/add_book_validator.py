0308a5e7a31ce447a16960e828224735f4cc7d9c
JuryReimis <80026660+JuryReimis@users.noreply.github.com>
2024-11-24 16:21:46 +0300

    Добавлена локализация через константы

--- validators/add_book_validator.py
+++ validators/add_book_validator.py
@@ -1,12 +1,17 @@
 import re
 from string import digits
 
+from config import LIBRARY_HEADERS
+from utils.colouring import ConsoleColors
+from utils.pretty_tables import PrettyTables
 from utils.search import Search
+from validators.base_validator import BaseValidator
 
 
-class AddBookValidator:
+class AddBookValidator(BaseValidator):
 
-    def __init__(self, title: str = None, author: str = None, year: str = None):
+    def __init__(self, title: str = None, author: str = None, year: str = None, localization=None):
+        super().__init__(localization)
         self._title = title
         self._author = author
         self._year = year
@@ -17,7 +22,10 @@ class AddBookValidator:
         search.init_search([title])
         searched_books = search.get_searched_books()
         if searched_books:
-            return "Уже есть книги с таким названием, проверьте, хотите ли вы продолжить"
+            pretty_table = PrettyTables(LIBRARY_HEADERS, searched_books)
+            return f"{ConsoleColors.colour_text(
+                self._localization.EXIST_TITLE_WARNING, 'WHITE')}{pretty_table.get_pretty_table(
+                )}"
         else:
             return None
 
@@ -27,16 +35,14 @@ class AddBookValidator:
         search.init_search(['&&&'.join((self._title, self._author, self._year))])
         searched_books = search.get_searched_books()
         if searched_books:
-            return (f"""!!!Вы пытаетесь добавить книгу, которая уже есть в базе данных 
-Название: {searched_books[0].get('title')}
-Автор: {searched_books[0].get('author')}
-Год издания: {searched_books[0].get('year')}
-Если здесь есть какая-то ошибка - обратитесь в технический отдел или воспользуйтесь \"Жестким\" добавлением\n""")
+            pretty_table = PrettyTables(LIBRARY_HEADERS, searched_books)
+            return f"{ConsoleColors.colour_text(
+                self._localization.EXIST_BOOK_ERROR, 'RED')}{pretty_table.get_pretty_table()}"
         return None
 
     def validate_year(self, year: str, bc_pattern: str = r'(\d+)?\s*(д\.н\.э\.|до н\.э\.|BC)?(.*)') -> None | str:
         self._year = year
-        error = 'В значении "Год" Присутствует неизвестный символ, который не является цифрой'
+        error = self._localization.NO_DIGIT_IN_YEAR
         if year == "Неизвестно":
             return None
         bc = re.search(bc_pattern, year)
@@ -49,4 +55,3 @@ class AddBookValidator:
 
     def transform_year(self, source: str = 'EN', target: str = 'RU') -> str:
         return self._year.replace('BC', 'до н.э.')
-
